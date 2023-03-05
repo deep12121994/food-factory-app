5be3f9189a0b5eae6c2f009ca51a53d98b0ed0b7
unknown <balasdeep@gmail.com>
2023-03-05 12:51:20 +0530

    image issue fixed

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,59 +1,60 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
-import {API_ID,API_KEY} from '../key'
-import Recipe from './Recipe';
-import Header from './Header';
-import ErrorPage from './DefaultError';
+import React, { useState } from "react";
+import Axios from "axios";
+// import { API_ID, API_KEY } from "../key";
+import Recipe from "./Recipe";
+import Header from "./Header";
+import ErrorPage from "./DefaultError";
+
+const API_KEY = "69dda36967d346305fd46ec967e3072e";
+const API_ID = "2da257cb";
 
 const Home = () => {
-   // console.log(API_ID," ",API_KEY)
-    const[searchRecipe,setSearchRecipe] = useState("");
-    const [recipes,setRecipes] = useState([]);
-    const [alert, setAlert] = useState("");
+  // console.log(API_ID," ",API_KEY)
+  const [searchRecipe, setSearchRecipe] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
 
-    const url = `https://api.edamam.com/search?q=${searchRecipe}$&app_id=${API_ID}&app_key=${API_KEY}`;
+  const url = `https://api.edamam.com/search?q=${searchRecipe}$&app_id=${API_ID}&app_key=${API_KEY}`;
 
-    const getRecipes = async () => {
-        const result = await Axios(url);
-        if (!result.data.more) {
-            setRecipes([]);
-            return setAlert("No food with such name");
-            
-        }
-        //console.log(result.data.hits)
-        setRecipes(result.data.hits);
-        setAlert("");
+  const getRecipes = async () => {
+    const result = await Axios(url);
+    if (!result.data.more) {
+      setRecipes([]);
+      return setAlert("No food with such name");
+    }
+    console.log("hits: ", result.data.hits);
+    setRecipes(result.data.hits);
+    setAlert("");
+  };
 
-    };
+  const onSubmitData = (e) => {
+    e.preventDefault();
+    getRecipes();
+  };
 
-    const onSubmitData = (e) => {
-        e.preventDefault();
-        getRecipes();
-    };
-
-    return(
-        <div className="main-header">
-            <Header/>
-            <form className="form-container" onSubmit={onSubmitData}>
-            {alert !== "" && <ErrorPage alert={alert} />}
-                <input type="text" 
-                className="search-bar" 
-                placeholder="Enter Recipe"
-                autoComplete="off"
-                val = {searchRecipe}
-                onChange = {e => setSearchRecipe(e.target.value)}
-                />
-                <button id="btn-search">Search</button>
-            </form>
-            <div className="recipe-container">
-                {recipes !== [] &&
-                    recipes.map(val => {
-                        return <Recipe recipeList={val} key={val.id}/>;
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="main-header">
+      <Header />
+      <form className="form-container" onSubmit={onSubmitData}>
+        {alert !== "" && <ErrorPage alert={alert} />}
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Enter Recipe"
+          autoComplete="off"
+          val={searchRecipe}
+          onChange={(e) => setSearchRecipe(e.target.value)}
+        />
+        <button id="btn-search">Search</button>
+      </form>
+      <div className="recipe-container">
+        {recipes !== [] &&
+          recipes.map((val) => {
+            return <Recipe recipeList={val} key={val.id} />;
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -1,27 +1,33 @@
-import React,{useState} from 'react';
-import RecipeDetails from './Ingredient';
+import React, { useState } from "react";
+import RecipeDetails from "./Ingredient";
 
+const Recipe = ({ recipeList }) => {
+  console.log("list: ", recipeList);
+  const [showIngredients, setShowIngredients] = useState(false);
+  const { label, image, url, ingredients } = recipeList.recipe; //destructuring
 
-const Recipe = ({recipeList}) => {
-    const [showIngredients, setShowIngredients] = useState(false);
-    const {label,image,url,ingredients} = recipeList.recipe; //destructuring
-
-    return(
-        recipeList["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-            <div className="recipe-card">
-                <img src={image} alt="Recipe image"/>
-                <h2>{label}</h2>
-                <div className="div-btn">
-                    <button id="ingrd-btn" 
-                    onClick={() => setShowIngredients(!showIngredients)}>Ingredients</button>                    
-                    <a href={url} target="_blank" rel="noopener noreferrer" id="recipe-btn">
-                        Recipe
-                    </a>
-                </div>
-                <div className="ingredient-details">{showIngredients && <RecipeDetails ingredientsList={ingredients}/>}</div>
-            </div>
-        )
-    )
-} 
+  return (
+    // recipeList["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+    <div className="recipe-card">
+      <img src={image} alt="Recipe image" />
+      <h2>{label}</h2>
+      <div className="div-btn">
+        <button
+          id="ingrd-btn"
+          onClick={() => setShowIngredients(!showIngredients)}
+        >
+          Ingredients
+        </button>
+        <a href={url} target="_blank" rel="noopener noreferrer" id="recipe-btn">
+          Recipe
+        </a>
+      </div>
+      <div className="ingredient-details">
+        {showIngredients && <RecipeDetails ingredientsList={ingredients} />}
+      </div>
+    </div>
+    // )
+  );
+};
 
 export default Recipe;
